6cf2e50fb8984ff563b60cd8b8e17c10c88cb6d5
denghe <denghe@hotmail.com>
2019-01-28 16:10:33 +0800

    testing

--- test_libgo_1/main.cpp
+++ test_libgo_1/main.cpp
@@ -1,5 +1,27 @@
-﻿int main()
+﻿#include <libgo/libgo.h>
+#include <chrono>
+#include <iostream>
+
+co_timer timer(std::chrono::milliseconds(1), &co_sched);
+size_t n = 0;
+void timerCB() {
+	++n;
+	timer.ExpireAt(std::chrono::milliseconds(1), timerCB);
+}
+int main()
 {
+	timerCB();
+
+	co_timer timer2(std::chrono::nanoseconds(1), &co_sched);
+	//co_timer timer2(std::chrono::milliseconds(1), &co_sched);
+	timer2.ExpireAt(std::chrono::seconds(1), [] 
+	{
+		std::cout << n << std::endl;
+	});
+
+	go[]{
+		std::cout << "xx\n";
+	};
+	co_sched.Start();
 	return 0;
 }
-
