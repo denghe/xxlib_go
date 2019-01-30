588a112035c8b6d35b8a52ff7900a9188fa207c2
denghe <denghe@hotmail.com>
2019-01-31 03:33:15 +0800

    writing

---
+++ test_asio_4/main.cpp
@@ -0,0 +1,20 @@
+﻿#include "asio.hpp"
+#include <iostream>
+#include <functional>
+
+std::mutex coutMutex;
+template<typename ...Args>
+void Cout(Args const& ... args) {
+	std::lock_guard<std::mutex> lg_mutex(coutMutex);
+	std::initializer_list<int> n{ ((std::cout << args), 0)... };
+	(void)(n);
+}
+template<typename ...Args>
+void CoutT(Args const& ... args) {
+	Cout("[", std::this_thread::get_id(), "] ", args...);
+}
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
