aff99fb660df9d477cfac5661e8cf88cec8d7e61
denghe <denghe@hotmail.com>
2019-02-28 16:12:31 +0800

    add BLOCK_SIZE cfg

--- test_uv_2/main.cpp
+++ test_uv_2/main.cpp
@@ -3,7 +3,9 @@
 #include <cstring>
 #include <cassert>
 
-template<size_t blockSize = 65536>
+#define BLOCK_SIZE 65536
+
+template<size_t blockSize = BLOCK_SIZE>
 struct BlockPool {
 	void* header = nullptr;
 	inline void* Alloc() {
@@ -43,9 +45,9 @@ int main() {
 		uv_tcp_init(server->loop, peer);
 		uv_accept(server, (uv_stream_t*)peer);
 		uv_read_start((uv_stream_t*)peer, [](uv_handle_t* h, size_t suggested_size, uv_buf_t* buf) {
-			assert(suggested_size == 65536);
+			assert(suggested_size <= BLOCK_SIZE);
 			buf->base = (char*)bp.Alloc() + sizeof(uv_write_t_ex);			// 保留 uv_write_t_ex 空间以便直接用于 write
-			buf->len = suggested_size - sizeof(uv_write_t_ex);				// read 到的数据放在后面
+			buf->len = BLOCK_SIZE - sizeof(uv_write_t_ex);					// read 到的数据放在后面
 		}, [](uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf) {
 			if (nread > 0) {
 				auto req = (uv_write_t_ex*)(buf->base - sizeof(uv_write_t_ex));
