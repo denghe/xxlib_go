dc7378da908cfea07ad905fdbb001d347cde3cb5
denghe <denghe@hotmail.com>
2019-03-07 23:23:22 +0800

    try fix double free

--- test_uv_a/main.cpp
+++ test_uv_a/main.cpp
@@ -10,22 +10,23 @@ int main(int argc, char* argv[]) {
 	listener->OnAccept = [&loop](auto& peer) {
 		xx::Cout(peer->kcp->conv, " accepted.\n");
 		auto timeouter = xx::TryMake<xx::UvTimer>(loop, 2000, 0);	// 两秒内没数据的连接杀掉
-		timeouter->OnFire = [timeouter, peer] {
-			if (!peer->Disposed()) {
+		auto timeouter_w = xx::Weak(timeouter);
+		timeouter->OnFire = [timeouter, peer_w = xx::Weak(peer)] {
+			if (auto peer = peer_w.lock()) {
 				peer->Dispose();
 				xx::Cout("timeout. peer dispose\n");
 			}
 			timeouter->OnFire = nullptr;	// kill timer
 		};
-		peer->OnReceivePush = [timeouter, peer](xx::Object_s&& msg)->int {
+		peer->OnReceivePush = [timeouter_w, peer](xx::Object_s&& msg)->int {
 			if (int r = peer->SendPush(msg)) return r;	// echo
 			//peer->Flush();
-			return timeouter->Restart();
+			return timeouter_w.lock()->Restart();
 		};
-		peer->OnReceiveRequest = [timeouter, peer](int const& serial, xx::Object_s&& msg)->int {
+		peer->OnReceiveRequest = [timeouter_w, peer](int const& serial, xx::Object_s&& msg)->int {
 			if (int r = peer->SendResponse(serial, msg)) return r;	// echo
 			//peer->Flush();
-			return timeouter->Restart();
+			return timeouter_w.lock()->Restart();
 		};
 	};
 	loop.Run();
